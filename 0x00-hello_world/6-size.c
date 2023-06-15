89e6c54e66fe540ae8c34aa279fce36040e55340
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-06-15 07:26:02 -0700

    Show bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ long int d;
 long long int e;
 float f;
 
-printf("Size of a char: %d byte(s)\n", sizeof(c));
-printf("Size of an int: %d byte(s)\n", sizeof(a));
-printf("Size of a long int: %d byte(s)\n", sizeof(d));
-printf("Size of long long int: %d byte(s)\n", sizeof(e));
-printf("Size of float: %d byte(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n",sizeof(c));
+printf("Size of an int: %d byte(s)\n",sizeof(a));
+printf("Size of a long int: %d byte(s)\n",sizeof(d));
+printf("Size of long long int: %d byte(s)\n",sizeof(e));
+printf("Size of float: %d byte(s)\n",sizeof(f));
 return (0);
 }
