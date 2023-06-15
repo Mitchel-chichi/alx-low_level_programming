ffaa5ca5f1b6b53c5d4beaa49dbec8c8be1e6512
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-06-15 05:53:20 -0700

    Using put to print message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program to print the message
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
