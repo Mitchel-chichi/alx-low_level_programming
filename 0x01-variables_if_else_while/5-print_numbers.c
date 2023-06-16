00f62c2c221d2ad7f9df4feb6c3aac3b5b73b68c
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-06-16 07:58:41 -0700

    Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print only single numbers of base ten
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d", a);
+printf("\n");
+return (0);
+}
