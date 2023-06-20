d1b2ddeb27a6e742592634584e10a76b86958610
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-06-20 12:26:23 -0700

    Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
- * jack_bauer - it prints jack bauer minutes of the day 
+ * jack_bauer - it prints jack bauer minutes of the day
  * starting from 00:00 to 23:59
- *
+*/
+
+void jack_bauer(void)
+{
+	int t, r;
+
+	t = 0;
+
+	while (t < 24)
+	{
+		r = 0;
+		while (r < 60)
+		{
+			_putchar((t / 10) + '0');
+			_putchar((t % 10) + '0');
+			_putchar('.');
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+			_putchar('\n');
+			r++;
+		}
+		t++;
+	}
+}
