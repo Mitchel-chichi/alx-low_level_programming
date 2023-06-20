eda10bfb89d1027c8f6487d900e93b5471036f74
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-06-20 15:57:05 -0700

    input

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,35 @@
 #include "main.h"
-
 /**
- * jack_bauer - it prints jack bauer minutes of the day
- * starting from 00:00 to 23:59
+ * jack_bauer - prints his every minutes of the day
+ * Return: ...
 */
 
 void jack_bauer(void)
 {
-	int t, r;
+	int q;
+	int w;
+	int x;
+	int y;
 
-	t = 0;
-
-	while (t <= 23)
+	for (q = 0; q <= 2; q++)
 	{
-		r = 0;
-		while (r <= 59)
-		{
-			_putchar((t / 10) + '0');
-			_putchar((t % 10) + '0');
-			_putchar('.');
-			_putchar((r / 10) + '0');
-			_putchar((r % 10) + '0');
-			_putchar('\n');
-			r++;
-		}
-		t++;
+	for (w = 0; w <= 9; w++)
+	{
+	if((q <= 1 && w <= 9) || (q <= 2 && w <= 3))
+	{
+	for (x = 0; x <= 5; x++)
+	{
+	for (y = 0; y <= 9; y++)
+	{
+	_putchar(q + '0');
+	_putchar(w + '0');
+	_putchar(58);
+	_putchar(x + '0');
+	_putchar(y + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
 	}
 }
