c51769d68a3797751413cf3f11400023508c3a36
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-06-22 03:00:09 -0700

    Numbers printed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - it prints number zero to nine
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+int a;
+
+for (a >= 0; a <= 9; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
