7efa0aad6cd4c59d791132086aa3a65dcc1f0673
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-06-22 14:30:06 -0700

    gggggg

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 {
 int z, h;
 
-for (z = 0; z <= 10; z++)
+for (z = 0; z < 9; z++)
 {
 for (h = 0; h <= 14; h++)
 {
-if (h >= 10)
+if (h > 9)
 {
 _putchar((h / 10) + '0');
 }
