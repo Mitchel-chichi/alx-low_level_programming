dbef6ac048216b79e2012d6c90f8e78a5af55b70
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-06-26 09:49:12 -0700

    task final

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,12 @@ int u = 0, v = 0;
 char temp;
 
 while (s[v++])
-{
 u++;
-}
-for (v = u -1; v >= u / 2; u--)
+
+for (v = u -1; v >= u / 2; v--)
+{
 temp = s[v];
 s[v] = s[u - v - 1];
 s[u - v - 1] = temp;
+}
 }
