2004d86fbb2c22f393f51d9448f27267f8beb527
Mitchel-chichi <mitchelkadenyiopili@gmail.com>
2023-07-03 04:52:39 -0700

    Rock Bottom

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - used for setting the pointers value to a character
+ * @s: the parameter that is a double pointer
+ * @to: - the parameter that is a pointer to character
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
